e65f10cd958e160a76dce5644ca7e3b49ea1d3e3
mauripsale <maurizio.ipsale@gmail.com>
2025-12-21 15:15:32 +0100

    fix: resolve syntax error in Separator.tsx caused by malformed echo command

--- components/common/Separator.tsx
+++ components/common/Separator.tsx
@@ -1,1 +1,5 @@
-import React from 'react';\n\nexport const Separator: React.FC = () => (\n  <div className="h-6 w-px bg-slate-800 hidden md:block" />\n);
+import React from 'react';
+
+export const Separator: React.FC = () => (
+  <div className="h-6 w-px bg-slate-800 hidden md:block" />
+);
